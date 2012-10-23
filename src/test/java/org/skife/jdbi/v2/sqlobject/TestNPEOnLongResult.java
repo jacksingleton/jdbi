5c5bb4e8f46c86ef6d4d8fdc643f4f73bc97c279
Brian McCallister <brianm@skife.org>
2012-10-22 19:07:36 -0600

    tests against postgres

---
+++ src/test/java/org/skife/jdbi/v2/sqlobject/TestNPEOnLongResult.java
@@ -0,0 +1,70 @@
+package org.skife.jdbi.v2.sqlobject;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+
+import java.sql.Connection;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class TestNPEOnLongResult
+{
+    private Handle handle;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2:mem:test");
+        DBI dbi = new DBI(ds);
+        handle = dbi.open();
+
+        handle.execute("create table something (id int primary key, name varchar(100))");
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        handle.execute("drop table something");
+        handle.close();
+    }
+
+    @Test
+    public void testFoo() throws Exception
+    {
+        Dao dao = handle.attach(Dao.class);
+        assertThat(dao.findByIdName("Brian"), nullValue());
+    }
+
+    @Test
+    public void testAgainstPostgresql() throws Exception
+    {
+        PGSimpleDataSource ds = new PGSimpleDataSource();
+        ds.setServerName("localhost");
+        ds.setDatabaseName("brianm");
+
+        DBI dbi = new DBI(ds);
+        Handle h = dbi.open();
+        h.execute("create table something (id int primary key, name varchar(100))");
+
+        Dao dao = h.attach(Dao.class);
+        assertThat(dao.findByIdName("Brian"), nullValue());
+
+        h.execute("drop table something");
+    }
+
+
+    public static interface Dao
+    {
+        @SqlQuery("select id from something where name = :name")
+        public Long findByIdName(@Bind("name") String name);
+    }
+
+}
